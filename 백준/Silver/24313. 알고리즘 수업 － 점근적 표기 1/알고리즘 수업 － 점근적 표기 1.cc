677310514a05ba369d988f9ed3271ffca1fe56cc
SSUHYUNKIM <43875264+SSUHYUNKIM@users.noreply.github.com>
2023-11-24 09:44:42 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int a1, a0, c, n;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    cin >> a1 >> a0;
+    cin >> c;
+    cin >> n;
+
+    if(a1 * n + a0 <= c * n && a1 <= c)
+        cout << 1 << '\n';
+    else
+        cout << 0 << '\n';
+
+    return 0;
+}
