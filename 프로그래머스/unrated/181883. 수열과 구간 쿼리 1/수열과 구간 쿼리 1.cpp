6beffcfffe07b6aeb7b009be4a990acfc755a895
SSUHYUNKIM <43875264+SSUHYUNKIM@users.noreply.github.com>
2023-12-05 10:57:45 +0900

    [level unrated] Title: 수열과 구간 쿼리 1, Time: 0.22 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
+    vector<int> answer;
+    for(auto query : queries)
+    {
+        for(int i = query[0]; i <= query[1]; i++)
+        {
+            arr[i]++;
+        }
+    }
+    answer = arr;
+    return answer;
+}
