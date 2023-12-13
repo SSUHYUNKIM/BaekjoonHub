1db6e245d19ac057495d0435d00df985ca9947a3
SSUHYUNKIM <43875264+SSUHYUNKIM@users.noreply.github.com>
2023-12-13 10:39:05 +0900

    [level unrated] Title: 소문자로 바꾸기, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181876. 소문자로 바꾸기/소문자로 바꾸기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string myString) {
+    string answer = "";
+    transform(myString.begin(), myString.end(), myString.begin(), ::tolower);
+    answer = myString;
+    return answer;
+}
