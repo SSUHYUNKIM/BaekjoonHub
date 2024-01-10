d953363326840d12be86e98f4d574d66c11d7eb0
SSUHYUNKIM <43875264+SSUHYUNKIM@users.noreply.github.com>
2024-01-10 22:54:19 +0900

    [level unrated] Title: 0 떼기, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string n_str) {
+    string answer = "";
+    answer = to_string(stoi(n_str));
+    return answer;
+}
