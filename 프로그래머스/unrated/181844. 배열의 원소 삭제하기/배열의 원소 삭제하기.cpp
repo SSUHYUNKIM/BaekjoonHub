bec75f9872fe22fdce9f15bc6dd3d2a6dd143354
SSUHYUNKIM <43875264+SSUHYUNKIM@users.noreply.github.com>
2024-01-11 20:29:54 +0900

    [level unrated] Title: 배열의 원소 삭제하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<int> delete_list) {
+    vector<int> answer;
+    for(auto a : arr) 
+    {
+        if (find(delete_list.begin(), delete_list.end(), a) == delete_list.end()) 
+            answer.push_back(a);
+    }
+    return answer;
+}
