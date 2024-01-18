8da741bf3d7a7681823dc3c8ffacb71c52347a4d
SSUHYUNKIM <43875264+SSUHYUNKIM@users.noreply.github.com>
2024-01-18 13:00:31 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 1.26 ms, Memory: 4.8 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.cpp
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.cpp
@@ -5,18 +5,5 @@ using namespace std;
 
 vector<vector<int>> solution(int n) {
     vector<vector<int>> answer;
-    for(int i = 0; i < n; i++)
-    {
-        vector<int> temp;
-        for(int j = 0; j < n; j++)
-        {
-            if(i == j)
-                temp.push_back(1);
-            else
-                temp.push_back(0);
-        }
-        answer.push_back(temp);
-        temp.clear();
-    }
     return answer;
 }
