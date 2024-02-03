fb65d57968775003a8c98ccb838c8a8196dc10df
SSUHYUNKIM <43875264+SSUHYUNKIM@users.noreply.github.com>
2024-02-03 20:35:36 +0900

    [level 0] Title: 주사위의 개수, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> box, int n) {
+    int answer = 1;
+    for(auto b : box)
+    {
+        answer *= b / n;
+    }
+    return answer;
+}
