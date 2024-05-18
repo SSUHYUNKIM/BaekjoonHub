8fcdd6531d70887575cf28c1d1cd65ef4be018e0
SSUHYUNKIM <43875264+SSUHYUNKIM@users.noreply.github.com>
2024-05-19 00:29:49 +0900

    [Bronze IV] Title: 과자, Time: 128 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int answer = K * N - M;
+        
+        if(answer < 0)
+            answer = 0;
+        
+        System.out.println(answer);
+    }
+}
