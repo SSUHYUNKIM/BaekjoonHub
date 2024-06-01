fd599667f376adf9bd775491ca7e3ed98bd249e7
SSUHYUNKIM <43875264+SSUHYUNKIM@users.noreply.github.com>
2024-06-02 01:35:33 +0900

    [Bronze IV] Title: 방학 숙제, Time: 128 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/5532. 방학 숙제/방학 숙제.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        double max = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int L = Integer.parseInt(br.readLine());
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+        int D = Integer.parseInt(br.readLine());
+        
+        max = Math.max((double)A/(double)C, (double)B / (double)D);
+        max = Math.ceil(max);
+        L -= (int)max;
+        
+        System.out.print(L);
+    }
+}
