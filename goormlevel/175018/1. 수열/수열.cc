672be3fc7506119ff6946076e644485809eabf99
SSUHYUNKIM <43875264+SSUHYUNKIM@users.noreply.github.com>
2024-06-14 22:41:05 +0900

    [goormlevel] Title: 수열, Time: 0.00 ms, Memory: 3.44 MB, Difficulty: 2 -BaekjoonHub

---
+++ goormlevel/175018/1. 수열/수열.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int fibo[100001];
+int main() {
+	int K;
+	fibo[1] = 0;
+	fibo[2] = 1;
+	cin >> K;
+	
+	for(int i = 3; i <= K; i++) {
+		fibo[i] = (fibo[i - 1] + fibo[i - 2]) % 1000000007;
+	}
+	
+	cout << fibo[K] % 1000000007 << '\n';
+	return 0;
+}
