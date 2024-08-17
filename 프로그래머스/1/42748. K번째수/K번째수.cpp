cb75b2ea4854e9d7a8f14898e1c933ad5ec5d67c
SSUHYUNKIM <43875264+SSUHYUNKIM@users.noreply.github.com>
2024-08-18 00:36:48 +0900

    [level 1] Title: K번째수, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -8,14 +8,10 @@ vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
     
     for(auto command : commands) {
-        vector<int> temp;
-        for(int i = command[0] - 1; i < command[1]; i++) {
-            temp.push_back(array[i]);
-        }
+        vector<int> temp(array.begin() + command[0] - 1, array.begin() + command[1]);
         sort(temp.begin(), temp.end());
         answer.push_back(temp[command[2] - 1]);
     }
-    
     
     return answer;
 }
