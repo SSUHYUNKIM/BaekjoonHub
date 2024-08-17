ad439b5331e49878d02e6276cd20c1e62720f9a1
SSUHYUNKIM <43875264+SSUHYUNKIM@users.noreply.github.com>
2024-08-17 23:13:15 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 39.05 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 32.4 MB, 시간: 47.30 ms
+메모리: 32.4 MB, 시간: 39.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 12일 00:59:28
+2024년 08월 17일 23:13:13
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -5,23 +5,21 @@
 using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
-    string answer = "";
     unordered_map<string, int> participant_map;
 
-    
-    for(auto part : participant) {
+    for (auto part : participant) {
         participant_map[part]++;
     }
-    
-    for(auto com : completion) {
-        participant_map[com]--;
+
+    for (auto comp : completion) {
+        participant_map[comp]--;
     }
-    
-    for(auto part : participant) {
-        if(participant_map[part] != 0) {
-            answer = part;
+
+    for (auto part : participant_map) {
+        if (part.second > 0) {
+            return part.first;
         }
     }
-    
-    return answer;
+
+    return ""; 
 }
