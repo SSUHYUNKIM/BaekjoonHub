da5533f8abb89b4363d9ceef514255994c2b55bc
SSUHYUNKIM <yellosuhyun@naver.com>
2024-11-21 11:48:13 +0900

    [level 1] Title: 평균 구하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    double sum = 0;
+    
+    for(auto a : arr) {
+        sum += a;
+    }
+    
+    return sum / arr.size();
+}
