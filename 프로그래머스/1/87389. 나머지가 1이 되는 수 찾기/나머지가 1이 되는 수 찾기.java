f39aa8cc93f7274bbcc0fcf9a58ca2a92087d2c6
SSUHYUNKIM <yellosuhyun@naver.com>
2024-11-21 16:42:11 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 5.88 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
@@ -3,12 +3,12 @@ class Solution {
         int answer = 0;
         
         for(int i = 2; i < n; i++) {
-            if(n % i == 1) { 
+            if(n % i == 1) {
                 answer = i;
                 break;
             }
         }
-
+        
         return answer;
     }
 }
