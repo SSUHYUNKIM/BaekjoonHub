79d2f6c4e8e14bb66cd881ac9646a7ec309d6255
SSUHYUNKIM <yellosuhyun@naver.com>
2024-11-27 19:20:54 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 3.65 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+    int sum = 0, temp = x;
+    
+    while(temp > 0) {
+        sum += temp % 10;
+        temp /= 10;
+    }
+    
+    return x % sum == 0;
+}
