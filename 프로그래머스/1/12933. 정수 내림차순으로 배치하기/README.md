b75851799311a21475e8be82e54c91c55dd73e4b
SSUHYUNKIM <yellosuhyun@naver.com>
2024-11-27 15:29:00 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.02 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/README.md
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 정수 내림차순으로 배치하기 - 12933 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12933?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12933?language=cpp) 
 
 ### 성능 요약
 
-메모리: 78.3 MB, 시간: 0.34 ms
+메모리: 4.17 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 20일 01:34:45
+2024년 11월 27일 15:28:57
 
 ### 문제 설명
 
--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
@@ -1,16 +1,13 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <cmath>
 
 using namespace std;
 
 long long solution(long long n) {
-    long long answer = 0;
-    string temp = to_string(n);
+    string nStr = to_string(n);
     
-    sort(temp.begin(), temp.end(), greater<>());
-    answer = stoll(temp); 
+    sort(nStr.begin(), nStr.end(), greater<>());
     
-    return answer;
+    return stoll(nStr);
 }
