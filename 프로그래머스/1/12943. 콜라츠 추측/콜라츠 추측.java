890963fc2e785c6054594f6d37cc562743adfce3
SSUHYUNKIM <yellosuhyun@naver.com>
2024-12-03 18:40:08 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.06 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int num) {
+        int answer = -1;
+        long current = num;
+
+        if(num == 1) {
+            return 0;
+        }
+
+        for(int i = 1; i <= 500; i++) {
+            if(current % 2 == 0) {
+                current /= 2;
+            } else {
+                current = current * 3 + 1;
+            }
+            if(current == 1) {
+                return i;
+            }
+        }
+        
+        return answer;
+    }
+}
