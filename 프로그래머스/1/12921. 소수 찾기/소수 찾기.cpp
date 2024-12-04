7b2f39cc7339c4af9d0fee9a43aa6b684c4107fb
SSUHYUNKIM <yellosuhyun@naver.com>
2024-12-04 13:16:28 +0900

    [level 1] Title: 소수 찾기, Time: 2.47 ms, Memory: 3.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    vector<bool> isPrime(n + 1, true);
+
+    isPrime[0] = false;
+    isPrime[1] = false;
+
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    for (int i = 2; i <= n; i++) {
+        if (isPrime[i]) {
+            answer++;
+        }
+    }
+
+    return answer;
+}
