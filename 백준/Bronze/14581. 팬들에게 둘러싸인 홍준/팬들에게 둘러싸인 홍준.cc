3ace4c4d8b942ba87b5dd17e138cf221a6e73c3b
SSUHYUNKIM <yellosuhyun@naver.com>
2025-12-01 23:35:45 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+string name;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
+    cin >> name;
+    
+    cout << ":fan::fan::fan:" << '\n';
+    cout << ":fan::" << name << "::fan:" << '\n';
+    cout << ":fan::fan::fan:" << '\n';
+    return 0;
+}
