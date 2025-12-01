09697cd760ce6d6cfa37db026d1fa7f5efd00be9
SSUHYUNKIM <yellosuhyun@naver.com>
2025-12-02 08:08:32 +0900

    [Bronze V] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long M, N;
+    
+    cin >> M >> N;
+    
+    if(M == N) {
+        cout << "1" << '\n';
+    } else {
+        cout << "0" << '\n';
+    }
+    
+    return 0;
+}
