9d79497e8cfa19d2fcdefe85d2ac5c48fd6f3d65
SSUHYUNKIM <yellosuhyun@naver.com>
2025-12-04 23:45:19 +0900

    [Bronze V] Title: 세금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20492. 세금/세금.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long N;
+    cin >> N;
+    
+    cout << N * 78 / 100 << " " << N * 956 / 1000 << '\n';
+    
+    return 0;
+}
