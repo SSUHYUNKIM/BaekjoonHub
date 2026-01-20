aecef49593ad2cb102b64c72a6ff310d2e0421be
SSUHYUNKIM <yellosuhyun@naver.com>
2026-01-20 21:53:07 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.cpp
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.cpp
@@ -5,8 +5,15 @@ using namespace std;
 
 int solution(int n) {
     int answer = 0;
-    answer += (n / 7);
-    if(n % 7 != 0)
-        answer++;
+    
+    if(n < 7) {
+        answer = 1;
+    }
+    else if(n % 7 == 0) {
+        answer = n / 7;
+    } else {
+        answer = n / 7 + 1;
+    }
+    
     return answer;
 }
