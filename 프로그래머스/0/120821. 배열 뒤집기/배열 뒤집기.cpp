63597132a449f9c349c6e02631d85262e0b64698
SSUHYUNKIM <yellosuhyun@naver.com>
2026-01-28 00:26:29 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.cpp
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.cpp
@@ -1,11 +1,13 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 vector<int> solution(vector<int> num_list) {
-    vector<int> answer;
-    for(int i = num_list.size() - 1; i >= 0; i--)
-        answer.push_back(num_list[i]);
+    vector<int> answer = num_list;
+    
+    reverse(answer.begin(), answer.end());
+    
     return answer;
 }
