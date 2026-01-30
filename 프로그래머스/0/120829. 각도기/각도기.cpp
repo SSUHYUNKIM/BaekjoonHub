99080161fc5b2e47450156770c030ba6ac6771bd
SSUHYUNKIM <yellosuhyun@naver.com>
2026-01-31 01:59:34 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.cpp
+++ 프로그래머스/0/120829. 각도기/각도기.cpp
@@ -4,12 +4,15 @@
 using namespace std;
 
 int solution(int angle) {
-    int answer = 1;
-    if(angle == 90)
+    int answer = 4;
+    
+    if(angle > 0 && angle < 90) {
+        answer = 1;
+    } else if (angle == 90) {
         answer = 2;
-    else if(angle > 90 && angle < 180)
+    } else if (angle > 90 && angle < 180) {
         answer = 3;
-    else if(angle == 180)
-        answer = 4;
+    }
+    
     return answer;
 }
