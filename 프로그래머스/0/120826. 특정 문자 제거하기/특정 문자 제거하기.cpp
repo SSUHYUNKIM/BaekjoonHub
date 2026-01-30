9ca2a8eb2f0ba67b89bc5e088d8b2e7c48f592ad
SSUHYUNKIM <yellosuhyun@naver.com>
2026-01-31 01:58:31 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.cpp
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.cpp
@@ -5,8 +5,12 @@ using namespace std;
 
 string solution(string my_string, string letter) {
     string answer = "";
-    for(int i = 0; i < my_string.size(); i++)
-        if(my_string[i] != letter[0]) 
+    
+    for(int i = 0; i < my_string.size(); i++) {
+        if(my_string[i] != letter[0]) {
             answer += my_string[i];
+        }
+    }
+    
     return answer;
 }
