bc5e8d1c25fa87a4d127ec351eaca17c7485f96d
SSUHYUNKIM <yellosuhyun@naver.com>
2026-02-01 13:06:35 +0900

    [level 0] Title: 배열 자르기, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.cpp
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.cpp
@@ -5,7 +5,10 @@ using namespace std;
 
 vector<int> solution(vector<int> numbers, int num1, int num2) {
     vector<int> answer;
-    for(int i = num1; i <= num2; i++)
+    
+    for(int i = num1; i <= num2; i++) {
         answer.push_back(numbers[i]);
+    }
+    
     return answer;
 }
