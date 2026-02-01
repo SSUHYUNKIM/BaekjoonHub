c95bc1037d391966a9a8471109c551ea20079740
SSUHYUNKIM <yellosuhyun@naver.com>
2026-02-01 13:11:57 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.cpp
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.cpp
@@ -5,9 +5,11 @@ using namespace std;
 
 string solution(int age) {
     string answer = "";
-    string str =  to_string(age);
+    string s = to_string(age);
     
-    for(int i = 0; i < str.size(); i++)
-        answer += (str[i] + 49);
+    for (char c : s) {
+        answer += (c - '0') + 'a';
+    }
+    
     return answer;
 }
